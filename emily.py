549f4c17f7e3cf0ce6015cc9e8f9d288081cc328
Shomiron DAS GUPTA <shomiron@netmonastery.com>
2015-06-20 15:17:57 +0530

    recordVideo - DONE

--- emily.py
+++ emily.py
@@ -6,9 +6,12 @@ def recordVideo(filename):
         camera.resolution = (800, 600)
         camera.start_preview()
         camera.start_recording(filename + '.h264')
-        camera.wait_recording(5)
         camera.capture(filename + '.jpg', use_video_port=True)
         camera.wait_recording(5)
         camera.stop_recording()
 
-recordVideo("newfile")
+def getFilename():
+    filename = "evid-" + time.strftime("%d%m%Y-%H%M%S")
+    return filename
+
+recordVideo(getFilename())
