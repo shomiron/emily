bc3b2c1b975635d6a7180ee1e9defeb37494c8c9
Shomiron DAS GUPTA <shomiron@netmonastery.com>
2015-06-20 15:50:34 +0530

    fliped-video

--- emily.py
+++ emily.py
@@ -1,17 +1,17 @@
 import time
 import picamera
 
+
 def recordVideo(filename):
     with picamera.PiCamera() as camera:
         camera.resolution = (800, 600)
+        camera.vflip = True
         camera.start_preview()
         camera.start_recording(filename + '.h264')
         camera.capture(filename + '.jpg', use_video_port=True)
-        camera.wait_recording(5)
+        camera.wait_recording(10)
         camera.stop_recording()
 
-def getFilename():
-    filename = "evid-" + time.strftime("%d%m%Y-%H%M%S")
-    return filename
 
-recordVideo(getFilename())
+filename = "evid-" + time.strftime("%d%m%Y-%H%M%S")
+recordVideo(filename)
