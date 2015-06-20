41942963da3cf90d1cc55f24e4e1e132f0b3ce74
Shomiron DAS GUPTA <shomiron@netmonastery.com>
2015-06-20 15:54:56 +0530

    resolution-change 640 480

--- emily.py
+++ emily.py
@@ -4,12 +4,13 @@ import picamera
 
 def recordVideo(filename):
     with picamera.PiCamera() as camera:
-        camera.resolution = (800, 600)
+        camera.resolution = (640, 480)
         camera.vflip = True
         camera.start_preview()
         camera.start_recording(filename + '.h264')
+        camera.wait_recording(5)
         camera.capture(filename + '.jpg', use_video_port=True)
-        camera.wait_recording(10)
+        camera.wait_recording(5)
         camera.stop_recording()
 
 
