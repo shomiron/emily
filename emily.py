901870e189f92ac23c8ea10bc43af882939df7e8
Shomiron DAS GUPTA <shomiron@netmonastery.com>
2015-06-20 15:57:56 +0530

    changing subdirectories

--- emily.py
+++ emily.py
@@ -7,9 +7,9 @@ def recordVideo(filename):
         camera.resolution = (640, 480)
         camera.vflip = True
         camera.start_preview()
-        camera.start_recording(filename + '.h264')
+        camera.start_recording("../video-records/" + filename + '.h264')
         camera.wait_recording(5)
-        camera.capture(filename + '.jpg', use_video_port=True)
+        camera.capture("../image-records/" + filename + '.jpg', use_video_port=True)
         camera.wait_recording(5)
         camera.stop_recording()
 
